f0c9fe31090e1e24dbd537046b41371c9140ff0f
Natthanon Manop <natthanon.manop@gmail.com>
2021-01-19 09:09:29 +0700

    Change module name, Remove println

--- internal/repositories/crud.go
+++ internal/repositories/crud.go
@@ -25,22 +25,13 @@ func NewCRUDRepository(m *database.MongoDatabase, c *config.Config) *CRUDReposit
 
 // Create ...
 func (crud *CRUDRepository) Create(collectionName string, m interface{}) (string, error) {
-	fmt.Println("AAAAA")
 	collection := crud.MONGO.Client.Database(crud.config.DatabaseName).Collection(collectionName)
-	fmt.Println("BBBBB")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	fmt.Println("CCCCC")
 	defer cancel()
 	result, err := collection.InsertOne(ctx, m)
-	fmt.Println("DDDDD")
-
 	if err != nil {
 		return "", err
 	}
-	fmt.Println("EEEEE")
-
 	id := fmt.Sprintf("%s", result.InsertedID)
-	fmt.Println("FFFFF")
-	fmt.Println(id)
 	return id, nil
 }
