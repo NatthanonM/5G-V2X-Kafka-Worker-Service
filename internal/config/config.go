5bab81096dd6fec2a533329ad9d0f14179f34688
Wiwan5 <grace-2540@hotmail.com>
2021-04-10 15:34:07 +0700

    open sasl config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,9 +7,8 @@ type Config struct {
 	Port           string `env:"PORT" envDefault:"18080"`
 	Topic          string `env:"Topic" envDefault:"kafka_accident"`
 	TopicDDS       string `env:"Topic" envDefault:"kafka_dds"`
-	Broker1Address string `env:"Broker1Address" envDefault:"localhost:9092"`
-	Broker2Address string `env:"Broker2Address" envDefault:"localhost:9093"`
-	Broker3Address string `env:"Broker3Address" envDefault:"localhost:9094"`
+	Broker1Address string `env:"Broker1Address" envDefault:"localhost:9093"`
+	Broker2Address string `env:"Broker2Address" envDefault:"localhost:9094"`
 	GroupID        string `env:"GroupID" envDefault:"my-group"`
 	// Declare Connection
 	DataManagementServiceConnection string `env:"DATA_MANAGEMENT_CONNECTION" envDefault:"127.0.0.1:8082"`
