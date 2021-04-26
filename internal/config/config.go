107efd8dd7dbc3ff3c77f56f91ee42a3529c6c04
Natthanon Manop <natthanon.manop@gmail.com>
2021-04-26 09:09:45 +0700

    Working on Containerization

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,18 +5,18 @@ import "github.com/caarlos0/env/v6"
 // Config ...
 type Config struct {
 	Port           string `env:"PORT" envDefault:"18080"`
-	Topic          string `env:"Topic" envDefault:"kafka_accident"`
-	TopicDDS       string `env:"Topic" envDefault:"kafka_dds"`
-	Broker1Address string `env:"Broker1Address" envDefault:"localhost:9092"`
-	Broker2Address string `env:"Broker2Address" envDefault:"localhost:9093"`
-	Broker3Address string `env:"Broker3Address" envDefault:"localhost:9094"`
-	GroupID        string `env:"GroupID" envDefault:"my-group"`
+	Topic          string `env:"TOPIC_ACCIDENT" envDefault:"kafka_accident"`
+	TopicDDS       string `env:"TOPIC_DROWSINESS" envDefault:"kafka_dds"`
+	Broker1Address string `env:"BROKER_1_ADDRESS" envDefault:"localhost:9092"`
+	Broker2Address string `env:"BROKER_2_ADDRESS" envDefault:"localhost:9093"`
+	Broker3Address string `env:"BROKER_3_ADDRESS" envDefault:"localhost:9094"`
+	GroupID        string `env:"GROUP_ID" envDefault:"my-group"`
 	// Declare Connection
 	DataManagementServiceConnection string `env:"DATA_MANAGEMENT_CONNECTION" envDefault:"127.0.0.1:8082"`
 	DatabaseURI                     string `env:"DATABASE_URI,file" envDefault:"./env/database_uri"`
 	DatabaseName                    string `env:"DATABASE_NAME,file" envDefault:"./env/database_name"`
-	UsernameKafka                   string `env:"DATABASE_NAME,file" envDefault:"./env/username"`
-	PasswordKafka                   string `env:"DATABASE_NAME,file" envDefault:"./env/password"`
+	UsernameKafka                   string `env:"USERNAME_KAFKA,file" envDefault:"./env/username"`
+	PasswordKafka                   string `env:"PASSWORD_KAFKA,file" envDefault:"./env/password"`
 }
 
 // NewConfig ...
